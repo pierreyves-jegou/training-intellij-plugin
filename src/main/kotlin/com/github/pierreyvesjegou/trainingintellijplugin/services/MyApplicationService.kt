90cdc3cc4b38feacdc33ad487f2f8c24010c1840
GitHub Action <action@github.com>
2022-04-22 16:12:16 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pierreyvesjegou/trainingintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pierreyvesjegou.trainingintellijplugin.services
+
+import com.github.pierreyvesjegou.trainingintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
